cdcb15d0543dca1069552122c6a98bdf2c404b4b
Kulcsár Bálint <kubalint@gmail.com>
2019-03-01 15:07:47 +0100

    Nyomtatás megy

--- scripts/PresentationLayer.js
+++ scripts/PresentationLayer.js
@@ -135,6 +135,22 @@ function generateHandler(){
 
 }
 
-function printHandler(){
-
-}
+function printHandler()
+    
+    {
+        var mywindow = window.open('', 'PRINT', 'height=400,width=600');
+    
+        mywindow.document.write('<html><head><title>' + document.title  + '</title>');
+        mywindow.document.write('</head><body >');
+        mywindow.document.write('<h1>' + document.title  + ' kivonat</h1>');
+        mywindow.document.write(document.getElementById("naplofelulet").innerHTML);
+        mywindow.document.write('</body></html>');
+    
+        mywindow.document.close(); // necessary for IE >= 10
+        mywindow.focus(); // necessary for IE >= 10*/
+    
+        mywindow.print();
+        mywindow.close();
+    
+        return true;
+    }
